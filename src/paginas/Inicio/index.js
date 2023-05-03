ad52de3eeef85d855b148350d42d6e316042ef3b
Juliana Lucca <lucca.editora@gmail.com>
2023-05-03 14:05:24 -0300

    create dynamic posts route

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Inicio.module.css";
-import Post from "componentes/Post";
+import Post from "componentes/PostCard";
 import posts from "json/posts.json";
 
 export default function Inicio() {
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
+import Post from "paginas/Post";
 
 function AppRoutes() {
   return (
@@ -12,8 +13,9 @@ function AppRoutes() {
 
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
-          <Route index element={<Inicio />}/>
-          <Route path="sobremim" element={<SobreMim />}/>
+          <Route index element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />
+          <Route path="posts/:id" element={<Post/>} />
         </Route>
 
         <Route path="*" element={<div>Página não encontrada</div>}/>
